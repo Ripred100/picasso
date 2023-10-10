f3633449b2849ef23948828b2a0314addb33aa46
Ripred100 <anuarsaenger.2001@gmail.com>
2023-10-10 15:32:09 -0400

    Initial commit

---
+++ cost_functions.py
@@ -0,0 +1,138 @@
+import tensorflow as tf
+
+@tf.function()
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+    
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """  
+    ### START CODE HERE
+    
+    #(≈1 line)
+    GA = tf.matmul(A, A, transpose_b=True)
+    
+    ### END CODE HERE
+
+    return GA
+
+@tf.function()
+def compute_content_cost(content_output, generated_output):
+    """
+    Computes the content cost
+    
+    Arguments:
+    a_C -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image C 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing content of the image G
+    
+    Returns: 
+    J_content -- scalar that you compute using equation 1 above.
+    """
+    a_C = content_output[-1]
+    a_G = generated_output[-1]
+    
+    ### START CODE HERE
+    
+    # Retrieve dimensions from a_G (≈1 line)
+    m, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Reshape 'a_C' and 'a_G' (≈2 lines)
+    # DO NOT reshape 'content_output' or 'generated_output'
+    a_C_unrolled = tf.reshape(a_C, shape=[m, -1, n_C])
+    a_G_unrolled = tf.reshape(a_G, shape=[m, -1, n_C])
+    
+    # compute the cost with tensorflow (≈1 line)
+    J_content = tf.reduce_sum(tf.square(tf.subtract(a_C_unrolled, a_G_unrolled))) / (4*n_H*n_W*n_C)
+    
+    ### END CODE HERE
+    
+    return J_content
+
+@tf.function()
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+    
+    Returns: 
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    ### START CODE HERE
+    
+    # Retrieve dimensions from a_G (≈1 line)
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Reshape the tensors from (1, n_H, n_W, n_C) to (n_C, n_H * n_W) (≈2 lines)
+    a_S = tf.reshape(tf.transpose(a_S,[3,0,1,2]), shape=[n_C, n_H*n_W])
+    a_G = tf.reshape(tf.transpose(a_G,[3,0,1,2]), shape=[n_C, n_H*n_W])
+
+    # Computing gram_matrices for both images S and G (≈2 lines)
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss (≈1 line)
+    J_style_layer = tf.reduce_sum(tf.square(tf.subtract(GS,GG)))/(4*(n_C**2) * ((n_H*n_W)**2))
+    ### END CODE HERE
+    
+    return J_style_layer
+
+@tf.function()
+def compute_style_cost(style_image_output, generated_image_output, STYLE_LAYERS):
+    """
+    Computes the overall style cost from several chosen layers
+    
+    Arguments:
+    style_image_output -- our tensorflow model
+    generated_image_output --
+    STYLE_LAYERS -- A python list containing:
+                        - the names of the layers we would like to extract style from
+                        - a coefficient for each of them
+    
+    Returns: 
+    J_style -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    
+    # initialize the overall style cost
+    J_style = 0
+
+    # Set a_S to be the hidden layer activation from the layer we have selected.
+    # The last element of the array contains the content layer image, which must not be used.
+    a_S = style_image_output[:-1]
+
+    # Set a_G to be the output of the choosen hidden layers.
+    # The last element of the list contains the content layer image which must not be used.
+    a_G = generated_image_output[:-1]
+    for i, weight in zip(range(len(a_S)), STYLE_LAYERS):  
+        # Compute style_cost for the current layer
+        J_style_layer = compute_layer_style_cost(a_S[i], a_G[i])
+
+        # Add weight * J_style_layer of this layer to overall style cost
+        J_style += weight[1] * J_style_layer
+
+    return J_style
+
+@tf.function()
+def total_cost(J_content, J_style, alpha = 10, beta = 40):
+    """
+    Computes the total cost function
+    
+    Arguments:
+    J_content -- content cost coded above
+    J_style -- style cost coded above
+    alpha -- hyperparameter weighting the importance of the content cost
+    beta -- hyperparameter weighting the importance of the style cost
+    
+    Returns:
+    J -- total cost as defined by the formula above.
+    """
+    ### START CODE HERE
+    
+    #(≈1 line)
+    J = alpha*J_content + beta*J_style
+    
+    ### START CODE HERE
+
+    return J
---
+++ misc_utils.py
@@ -0,0 +1,41 @@
+
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+def get_layer_outputs(vgg, layer_names):
+    """ Creates a vgg model that returns a list of intermediate output values."""
+    outputs = [vgg.get_layer(layer[0]).output for layer in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+def clip_0_1(image):
+    """
+    Truncate all the pixels in the tensor to be between 0 and 1
+    
+    Arguments:
+    image -- Tensor
+    J_style -- style cost coded above
+
+    Returns:
+    Tensor
+    """
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+def tensor_to_image(tensor):
+    """
+    Converts the given tensor into a PIL image
+    
+    Arguments:
+    tensor -- Tensor
+    
+    Returns:
+    Image: A PIL image
+    """
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return Image.fromarray(tensor)

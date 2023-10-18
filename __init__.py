411ebf7c94fcdbc8079f6002cf52b17026fd035c
Your Name <anuarsaenger.2001@gmail.com>
2023-10-17 20:24:37 -0400

    fixed style weights in picasso.__init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,16 @@
 STYLE_LAYERS = [
-    ('block1_conv1', 1),#200
+    ('block1_conv1', 0.2),#200
     #('block1_conv2', 0.2),
-    ('block2_conv1', 1),#20
+    ('block2_conv1', 0.2),#20
     #('block2_conv2', 0.2),
-    ('block3_conv1', 1),#20
+    ('block3_conv1', 0.2),#20
     #('block3_conv2', 0.2),
     #('block3_conv3', 0.2),
-    ('block4_conv1', 1),#0.00002
+    ('block4_conv1', 0.2),#0.00002
     #('block4_conv2', 0.2),
     #('block4_conv3', 0.2),
     #('block4_conv4', 0.2),
-    ('block5_conv1', 1),
+    ('block5_conv1', 0.2),
     #('block5_conv2', 0.2),
     #('block5_conv3', 0.2),
     #('block5_conv4', 0.2)
